e355b754eb859782258b0b4d76ae8fef4bf30fd3
Hemang Shishir <hshishir@outlook.com>
2016-11-28 23:23:11 -0800

    Observer pattern.

---
+++ src/ObserverPattern/WeatherData.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    public class WeatherData : ISubject
+    {
+        private float _temparature;
+        private float _humidity;
+        private float _pressure;
+        private List<IObserver> observers;
+
+        public WeatherData()
+        {
+            observers = new List<IObserver>();
+        }
+
+        public float GetTemparature()
+        {
+            return _temparature;
+        }
+
+        public float GetHumidity()
+        {
+            return _humidity;
+        }
+
+        public float GetPressure()
+        {
+            return _pressure;
+        }
+
+        public void RegisterObserver(IObserver o)
+        {
+            observers.Add(o);
+        }
+
+        public void NotifyObersevers()
+        {
+            foreach (var observer in observers)
+            {
+                observer.Update(_temparature, _humidity, _pressure);
+            }
+        }
+
+        public void RemoveObserver(IObserver o)
+        {
+            observers.Remove(o);
+        }
+
+        public void SetWeatherData(float temperature, float humidity, float pressure)
+        {
+            _temparature = temperature;
+            _humidity = humidity;
+            _pressure = pressure;
+            NotifyObersevers();
+        }
+    }
+}
---
+++ src/ObserverPattern/WeatherStatisticsDisplay.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    public class WeatherStatisticsDisplay : IObserver, IDisplay
+    {
+        private WeatherData _weatherData;
+        private float _temperature;
+        private float _humidity;
+        private float _pressure;
+
+        public WeatherStatisticsDisplay(WeatherData weatherData)
+        {
+            _weatherData = weatherData;
+            _weatherData.RegisterObserver(this);
+        }
+        public void Display()
+        {
+            Console.WriteLine("Weather Statistics Display");
+            Console.WriteLine($"Temperature: {_temperature} Humidity {_humidity} Presssure: {_pressure} ");
+        }
+
+        public void Update(float temperature, float humidity, float pressure)
+        {
+            _temperature = temperature;
+            _humidity = humidity;
+            _pressure = pressure;
+
+            Display();
+        }
+    }
+}
